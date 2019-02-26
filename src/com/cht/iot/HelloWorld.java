f74b206642a76b1c691153c4516e161d272dcaff
HuaLinWoo <xup3016@gmail.com>
2019-02-26 15:15:01 +0800

    test

---
+++ src/com/cht/iot/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.cht.iot;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class HelloWorld {
+	private static Logger logger = LoggerFactory.getLogger(HelloWorld.class);
+	
+	public static void main(String[] args) {
+		logger.info("Hello World!");
+	}
+
+}
